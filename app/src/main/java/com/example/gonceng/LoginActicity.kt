e9d385426384f27f7ad82885e1a269aa348bc892
hannnzu <zyrexxzz@gmail.com>
2025-11-26 19:23:13 +0700

    menambahkan file activity untuk login dan register

--- app/src/main/java/com/example/gonceng/LoginActicity.kt
+++ app/src/main/java/com/example/gonceng/LoginActicity.kt
@@ -1,32 +1,19 @@
 package com.example.gonceng
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActicity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val btnlogin = findViewById<Button>(R.id.btnLogin)
-        val buttonregister = findViewById<Button>(R.id.btnRegister)
-
-        btnlogin.setOnClickListener {
-            // Habib
-        }
-
-        buttonregister.setOnClickListener {
-            // Alpian
-        }
-
+        setContentView(R.layout.activity_login_acticity)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
