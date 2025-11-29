ecb6661b51b074263bc067660a09f14ad9f404bc
habibhajid <habibhajid987@gmail.com>
2025-11-30 06:16:35 +0700

    Mengganti Logo App, Membuat halaman trip/input lokasi

--- app/src/main/java/com/example/gonceng/HomeFragment.kt
+++ app/src/main/java/com/example/gonceng/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.example.gonceng
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +38,16 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button_mobil : ImageView = view.findViewById(R.id.gambar_mobil)
+        button_mobil.setOnClickListener {
+            val intent = Intent(requireContext(), InputLokasi::class.java)
+            startActivity(intent)
+        }
     }
 
     companion object {
