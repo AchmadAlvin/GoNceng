0031b621ffd36a5cc16bdbc11c67130ca6404759
hannnzu <zyrexxzz@gmail.com>
2025-12-04 18:40:25 +0700

    membuat halaman oder makanan serta styling

--- app/src/main/java/com/example/gonceng/OrderDetailGeprekJuara.kt
+++ app/src/main/java/com/example/gonceng/OrderDetailGeprekJuara.kt
@@ -2,29 +2,22 @@ package com.example.gonceng
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class DetailGeprek : AppCompatActivity() {
+class OrderDetailGeprekJuara : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detail_geprek)
+        setContentView(R.layout.activity_order_detail_geprek_juara)
 
-        val btnBackFood = findViewById<CardView>(R.id.btnBackFood)
-        val btnOrderGeprekJuara = findViewById<LinearLayout>(R.id.btnOrderGeprekJuara)
+        val btnOrder = findViewById<Button>(R.id.btnOrder)
 
-        btnBackFood.setOnClickListener {
-            finish()
-        }
-
-
-        btnOrderGeprekJuara.setOnClickListener {
-            val intent = Intent(this, OrderDetailGeprekJuara::class.java)
+        btnOrder.setOnClickListener {
+            val intent = Intent(this, PesananBerhasilDibuat::class.java)
             startActivity(intent)
         }
 
