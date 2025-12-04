370ad1b26cf4fc6231d18b8e652f7bfd6dd71088
hannnzu <zyrexxzz@gmail.com>
2025-12-04 08:18:34 +0700

    styling halaman makanan

--- app/src/main/java/com/example/gonceng/FoodActivity.kt
+++ app/src/main/java/com/example/gonceng/FoodActivity.kt
@@ -3,6 +3,7 @@ package com.example.gonceng
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,6 +12,13 @@ class FoodActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_food)
+
+        val btnBack = findViewById<CardView>(R.id.btnBack)
+
+        btnBack.setOnClickListener {
+            finish()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
