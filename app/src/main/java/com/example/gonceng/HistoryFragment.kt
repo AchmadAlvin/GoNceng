3291a0bb5be5f51e331574b63047ee07796aa647
AchmadAlvin <achmadalvin886@gmail.com>
2025-12-11 08:06:18 +0700

    history with dummy

--- app/src/main/java/com/example/gonceng/HistoryFragment.kt
+++ app/src/main/java/com/example/gonceng/HistoryFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gonceng.HistoryItem
+import com.example.gonceng.HistoryAdapter
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +39,26 @@ class HistoryFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_history, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val rv = view.findViewById<RecyclerView>(R.id.rvHistoryList)
+
+        val historyList = listOf(
+            HistoryItem("6 KM", "Politeknik Negeri Madiun o Demangan, Taman", R.drawable.motor),
+            HistoryItem("7 KM", "Politeknik Negeri Madiun o Dagangan, dagangan", R.drawable.mobil),
+            HistoryItem("1 KM", "Politeknik Negeri Madiun o Universitas Merdeka", R.drawable.motor),
+            HistoryItem("8 KM", "Politeknik Negeri Madiun o Universitas PGRI", R.drawable.mobil),
+            HistoryItem("8 KM", "Politeknik Negeri Madiun o Politeknik Negeri Madiun", R.drawable.motor),
+            HistoryItem("8 KM", "Politeknik Negeri Madiun o SMK 8 Madiun", R.drawable.mobil),
+            HistoryItem("12 KM", "Politeknik Negeri Madiun o Pasar Besar", R.drawable.motor),
+            HistoryItem("4 KM", "Politeknik Negeri Madiun o Alun-Alun Madiun", R.drawable.mobil)
+        )
+
+        rv.adapter = HistoryAdapter(historyList)
+        rv.layoutManager = LinearLayoutManager(requireContext())
     }
 
     companion object {
