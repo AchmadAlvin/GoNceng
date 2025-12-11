28f1b87d5fd6e157e218cdae5b94ecad9cfd9553
habibhajid <habibhajid987@gmail.com>
2025-12-11 08:07:58 +0700

    membuat intent menuju pesanan berhasil dibuat

---
+++ app/src/main/java/com/example/gonceng/DetailPesananMotor.kt
@@ -0,0 +1,30 @@
+package com.example.gonceng
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetailPesananMotor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail_pesanan_motor)
+
+        val buttob_pesan : Button = findViewById<Button>(R.id.button_pesan)
+
+        buttob_pesan.setOnClickListener {
+            val intent = Intent(this, PesananBerhasilDibuat::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
