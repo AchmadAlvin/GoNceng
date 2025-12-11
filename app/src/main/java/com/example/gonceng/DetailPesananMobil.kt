818cc0c9e7eb34d3975623d476287a3845026f85
habibhajid <habibhajid987@gmail.com>
2025-12-11 19:43:05 +0700

    inisialisasi sintaks halaman

--- app/src/main/java/com/example/gonceng/DetailPesananMobil.kt
+++ app/src/main/java/com/example/gonceng/DetailPesananMobil.kt
@@ -16,22 +16,22 @@ class DetailPesananMobil : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_detail_pesanan_mobil)
+//      inisilisasi variable layout
         val button_pesan : Button = findViewById<Button>(R.id.button_pesan)
 
         val lokasi_tujuan : TextView = findViewById<TextView>(R.id.text_data_lokasi_tujuan)
 
-        val data_lokasi_tujuan = intent.getStringExtra("data_lokasi_tujuan")
-
         val back_button : ImageView = findViewById<ImageView>(R.id.back_button)
-
-
+//      variable yang menagmbil data dari inputlkasi yang putextra parameter yakni key yang ada di input
+        val data_lokasi_tujuan = intent.getStringExtra("data_lokasi_tujuan")
+//      merubah textview menjadi text
         lokasi_tujuan.text = data_lokasi_tujuan
-
+//      button pesn udah paha
         button_pesan.setOnClickListener {
             val intent = Intent(this, PesananBerhasilDibuat::class.java)
             startActivity(intent)
         }
-
+//      button back udah paham
         back_button.setOnClickListener {
             val intent = Intent(this, InputLokasiMobil::class.java)
             startActivity(intent)
