87f39d152122e4f20eb43b1a41b29ca16bd0048e
hannnzu <zyrexxzz@gmail.com>
2025-12-11 11:47:16 +0700

    implementasi food adapter

--- app/src/main/java/com/example/gonceng/Activity/FoodActivity.kt
+++ app/src/main/java/com/example/gonceng/Activity/FoodActivity.kt
@@ -7,7 +7,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gonceng.Adapter.FoodAdapter
 import com.example.gonceng.DetailFood
+import com.example.gonceng.Model.FoodItem
 import com.example.gonceng.R
 
 class FoodActivity : AppCompatActivity() {
@@ -17,23 +21,35 @@ class FoodActivity : AppCompatActivity() {
         setContentView(R.layout.activity_food)
 
         val btnBack = findViewById<CardView>(R.id.btnBack)
-        val btnGeprek = findViewById<CardView>(R.id.btnGeprek)
-        val btnMieAyam = findViewById<CardView>(R.id.btnMieAyam)
-
+        val rvFood = findViewById<RecyclerView>(R.id.rvFood)
 
         btnBack.setOnClickListener {
             finish()
         }
 
-        btnGeprek.setOnClickListener {
+        // data dummy menu
+        val foodList = listOf(
+            FoodItem(
+                name = "Ayam Geprek Masbro",
+                details = "Diantar dalam 15-25 min • 1.0 km",
+                imageResId = R.drawable.geprek
+            ),
+            FoodItem(
+                name = "Mie Ayam Pak Bas",
+                details = "Diantar dalam 15-25 min • 0.5 km",
+                imageResId = R.drawable.mi_ayam
+            )
+        )
+
+        // Adapter
+        val adapter = FoodAdapter(foodList) { food ->
             val intent = Intent(this, DetailFood::class.java)
             startActivity(intent)
         }
 
-        btnMieAyam.setOnClickListener {
-            val intent = Intent(this, DetailFood::class.java)
-            startActivity(intent)
-        }
+        // Set RecyclerView
+        rvFood.layoutManager = LinearLayoutManager(this)
+        rvFood.adapter = adapter
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
